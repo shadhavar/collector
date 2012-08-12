b7d7a73e509587dd9d7dc41db74bf17c4812d24a
Bas Pape <baspape@gmail.com>
2012-08-12 22:24:28 +0200

    Increase readability of collector.py

--- collector.py
+++ collector.py
@@ -14,22 +14,19 @@ def signal_main(signum, frame):
     exit(0)
 
 def signal_plugin(signum, frame):
-    print "Exiting plugin {0}".format(instance)
+    global m
+    print "Exiting plugin {0}".format(sys.argv[1])
     m.shutdown = True
 
-if not __name__ == "__main__":
-    print "The collector should not be imported as a module"
-    os.exit(1)
+def load_config():
+    print "Loading config"
+    f = open("config", "r")
+    conf = json.loads(f.read())
+    f.close()
+    return conf
 
-# Load config
-print "Loading config"
-f = open("config", "r")
-conf = json.loads(f.read())
-socketpath = conf["socketpath"]
-
-# if passed an argument, we run that plugin instance
-if len(sys.argv) > 1:
-    instance = sys.argv[1]
+def run_plugin(instance):
+    global m
     if not instance in conf["plugins"]:
         print "No configuration for {0}, exiting.".format(name)
         os.exit(1)
@@ -49,8 +46,8 @@ if len(sys.argv) > 1:
     m.stop()
 
 
-else:
-    # create a unix domain socket to be used as sink
+# create a unix domain socket to be used as sink
+def initialize_socket():
     print "Preparing domain socket"
     sink = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
     try:
@@ -59,18 +56,30 @@ else:
         pass
 
     sink.bind(socketpath)
+    return sink
 
-    # Prepare modules and plugins
-    processes = {}
-    #TODO see if stuff gets lost when starting takes a while
-    for name in conf["plugins"].keys():
-        print "Starting collector for plugin {0}".format(name)
-        processes[name] = subprocess.Popen([sys.executable, sys.argv[0], name])
 
-    #TODO regularly poll() the subprocesses
+if __name__ == "__main__":
+    conf = load_config()
+    socketpath = conf["socketpath"]
 
-    signal.signal(signal.SIGTERM, signal_main)
+    # if passed an argument, we run that plugin instance
+    if len(sys.argv) > 1:
+        run_plugin(sys.argv[1])
+    else:
+        sink = initialize_socket()
 
-    while True:
-        data, address = sink.recvfrom(4096) # hmm
-        print >>sys.stderr, data
+        # Prepare modules and plugins
+        processes = {}
+        #TODO see if stuff gets lost when starting takes a while
+        for name in conf["plugins"].keys():
+            print "Starting collector for plugin {0}".format(name)
+            processes[name] = subprocess.Popen([sys.executable, sys.argv[0], name])
+
+        #TODO regularly poll() the subprocesses
+
+        signal.signal(signal.SIGTERM, signal_main)
+
+        while True:
+            data, address = sink.recvfrom(4096) # hmm
+            print >>sys.stderr, data
